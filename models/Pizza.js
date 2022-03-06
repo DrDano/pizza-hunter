62c3aeb7bb146ed9bff011651738a4068b4246dd
DrDano <danielharned@gmail.com>
2022-03-05 17:11:03 -0800

    adding comments reference to pizza model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,23 +1,41 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-      type: String
+      type: String,
     },
     createdBy: {
-      type: String
+      type: String,
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
     },
     size: {
       type: String,
-      default: 'Large'
+      default: "Large",
     },
-    toppings: []
-  });
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-const Pizza = model('Pizza', PizzaSchema);
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
+});
+
+const Pizza = model("Pizza", PizzaSchema);
 
 module.exports = Pizza;
